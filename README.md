0f890ff467ac9daf0db5862aa468e2fc961f427f
oscar50 <oscar@whaleandjaguar.co>
2022-05-31 08:04:37 -0500

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Celo_test
 Pruebas iniciales con kit develop Celo para python
+
+![alt text](https://photos.app.goo.gl/eMHUiAB5q4T7Lnfe7)
--- dashboard.py
+++ dashboard.py
@@ -58,7 +58,8 @@ fig.update_layout(template=template, title="Temperatura C, Humedad %")
 fig_json = fig.to_json()
 
 # a simple HTML template
-template = """<html>
+plotly_data = 'var plotly_data = {}'
+template = f"""<html>
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
@@ -73,7 +74,7 @@ template = """<html>
         <div class="mb-3 border rounded" style='padding: 16px;'>
             <div id='divPlotly'></div>
             <script>
-                var plotly_data = {}
+                {plotly_data}
                 Plotly.react('divPlotly', plotly_data.data, plotly_data.layout);
             </script>
         </div>
